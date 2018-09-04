c32b5e02786b7a0315407975b5d6ea09dc23391f
apurva13 <apurva13598@gmail.com>
2018-09-04 22:59:46 +0530

    assignment 13

--- api.py
+++ api.py
@@ -1,10 +1,5 @@
 #Q.1-Use the "https://api.forismatic.com/api/1.0/" api to get random quotes using the correct endpoints.
 
 import requests
-response=requests.get("https://api.forismatic.com/api/1.0/")
-print(response.status_code)
-print(response.content)
-import json
-data=response.json()
-print(type(data))
-print(data)
+info=requests.get("https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?")
+print(info.content)
